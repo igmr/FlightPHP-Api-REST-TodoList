f0f6934e419cb8397d313f1c0168f071274b2294
= <=>
2023-01-23 17:22:35 -0600

    Flight API-REST - To do list - v.0.0.19 - Actualización de rutas de Home.

--- App/Components/v1/Home/HomeRoute.php
+++ App/Components/v1/Home/HomeRoute.php
@@ -7,6 +7,6 @@ $homeController = new HomeController();
 //*	RUTAS http://localshot:8081/
 //*	RUTAS http://localshot:8081/api
 //*	*****************************************************************************
-Flight::route('/', array($homeController, 'index'));
-Flight::route('/api/', array($homeController, 'index'));
-Flight::route('/api/v1', array($homeController, 'index'));
+Flight::route('/'        ,  array($homeController, 'index'));
+Flight::route('/api/'    ,  array($homeController, 'index'));
+Flight::route('/api/v1'  ,  array($homeController, 'index'));
