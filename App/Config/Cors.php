1b09bbe7a70df3190640909debb8933220ad7a35
= <=>
2023-01-24 16:19:31 -0600

    Flight API-REST - To do list - v.0.0.25 - Actualización Cors.php

--- App/Config/Cors.php
+++ App/Config/Cors.php
@@ -2,6 +2,8 @@
 
 Flight::before('json', function () {
     header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE');
-    header('Access-Control-Allow-Headers: Content-Type');
+    header('Access-Control-Allow-Methods: GET,HEAD,PUT,PATCH,POST,DELETE');
+    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+    header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization');
 });
+
