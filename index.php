b4671b737e8bf2579dee68e67733b7bbe0b11438
= <=>
2023-01-24 16:20:13 -0600

    Flight API-REST - To do list - v.0.0.26 - Headers agregados

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
 //*	Carga de archivos														*
 //*	*****************************************************************************
 
+// required headers
+
+require_once __DIR__.'/App/Config/Headers.php';
 require_once __dir__.'/Autoload.php';
 require_once __dir__.'/App/Config/Config.php';
 require_once __dir__.'/App/Config/Routes.php';
-require_once __DIR__.'/App/Config/Cors.php';
+//require_once __DIR__.'/App/Config/Cors.php';
 
 Flight::start();
